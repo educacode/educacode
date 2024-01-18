ec4d016a84840740d662665ec5a3e20fca358b20
Adriano <adrianovendramini@hotmail.com>
2024-01-18 14:15:56 -0300

    Task3

---
+++ src/main/java/com/generation/educacode/EducacodeApplication.java
@@ -0,0 +1,13 @@
+package com.generation.educacode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EducacodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EducacodeApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/generation/educacode/model/Categoria.java
@@ -0,0 +1,39 @@
+package com.generation.educacode.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_categorias")
+public class Categoria {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@NotNull(message = "O tipo é obrigatorio.")
+	@Size(min = 2, max = 100, message = "O atributo tipo deve conter no mínimo 05 e no máximo 100 caracteres")
+	private String tipo;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	
+}
